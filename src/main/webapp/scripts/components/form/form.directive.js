794c7310b491d3deb2a1802b46808c52848ccce0
xiaoyao9184 <xiaoyao9184@gmail.com>
2019-03-18 15:09:02 +0800

    Fix dynamic form validation

--- src/main/webapp/scripts/components/form/form.directive.js
+++ src/main/webapp/scripts/components/form/form.directive.js
@@ -3,6 +3,18 @@
 
 angular.module('uaaUIApp')
     .directive('showValidation', function() {
+        var watchInvalidAndDirty = function($scope,$inputs,$watchers,$toggle){
+            $inputs.each(function() {
+                var $input = $(this);
+                var watch = $scope.$watch(function() {
+                    return $input.hasClass('ng-invalid') && $input.hasClass('ng-dirty');
+                }, function(isInvalid) {
+                    $toggle.toggleClass('has-error', isInvalid);
+                });
+                $watchers.push(watch);
+            });
+        }
+
         return {
             restrict: 'A',
             require: 'form',
@@ -12,13 +24,31 @@ angular.module('uaaUIApp')
                     var $inputs = $formGroup.find('input[ng-model],textarea[ng-model],select[ng-model]');
 
                     if ($inputs.length > 0) {
-                        $inputs.each(function() {
-                            var $input = $(this);
-                            scope.$watch(function() {
-                                return $input.hasClass('ng-invalid') && $input.hasClass('ng-dirty');
-                            }, function(isInvalid) {
-                                $formGroup.toggleClass('has-error', isInvalid);
+                        var warchers = [];
+                        watchInvalidAndDirty(scope,$inputs,warchers,$formGroup);
+                    }else{
+                        var warchers = [];
+                        var $formInline;
+                        scope.$watch(function() {
+                            $inputs = $formGroup.find('input[ng-model],textarea[ng-model],select[ng-model]');
+                            return $inputs.length;
+                        }, function() {
+                            //remove all warcher
+                            warchers.forEach(function(warcher) {
+                                warcher();
                             });
+                            //add all warcher
+                            $formInline = $formGroup.find('div.form-inline');
+                            if($formInline.length > 0){
+                                $formInline.each(function() {
+                                    var $inline = $(this);
+                                    $inputs = $inline.find('input[ng-model],textarea[ng-model],select[ng-model]');
+                                    
+                                    watchInvalidAndDirty(scope,$inputs,warchers,$inline);
+                                });
+                                return;
+                            }
+                            watchInvalidAndDirty(scope,$inputs,warchers,$formGroup);
                         });
                     }
                 });
--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -4,6 +4,9 @@ angular.module('uaaUIApp')
     .controller('NavbarController', function ($scope, $state, Principal, ZoneHolder, ENV) {
         $scope.$state = $state;
         $scope.inProduction = ENV === 'prod';
+        
+        //revert
+        Principal.identity(true);
 
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.isClient = Principal.isClient;
