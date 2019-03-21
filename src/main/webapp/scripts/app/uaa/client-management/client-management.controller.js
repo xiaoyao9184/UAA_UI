6ac0e1dff7c298e092d0f190722055707bd0feb3
xiaoyao9184 <xiaoyao9184@gmail.com>
2019-03-21 14:37:43 +0800

    Support client go to Group detail by scope or authoritie Support client go to go IdentityProvider detail by scope or authoritie

--- src/main/webapp/scripts/app/uaa/client-management/client-management.controller.js
+++ src/main/webapp/scripts/app/uaa/client-management/client-management.controller.js
@@ -1,7 +1,11 @@
 'use strict';
 
 angular.module('uaaUIApp')
-    .controller('ClientManagementController', function ($scope, $q, Client, ClientMeta, Base64, GRANTS, GROUPS) {
+    .controller('ClientManagementController', 
+    function ($scope, $q, $state, $filter,
+        Client, ClientMeta, 
+        Group, IdentityProvider,
+        AlertService, Base64, SetUtils, GRANTS, GROUPS) {
         $scope.apps = [];
         $scope.loadAllApp = function () {
             return ClientMeta.query().$promise
@@ -40,7 +44,75 @@ angular.module('uaaUIApp')
             $scope.editForm.$setUntouched();
         };
 
+        
+        $scope.getScopeLabel = function(scope, autoapprove){
+            if(SetUtils.hasItem(autoapprove,'true')){
+                return 'label-success';
+            }
+            if(scope.indexOf('*') != -1){
+                return 'label-danger';
+            }
+            if(autoapprove.indexOf(scope) != -1){
+                return 'label-success';
+            }
+            return 'label-primary';
+        };
+        $scope.getScopeIcon = function(scope, autoapprove){
+            if(SetUtils.hasItem(autoapprove,'true')){
+                return 'glyphicon-ok';
+            }
+            if(scope.indexOf('*') != -1){
+                return 'glyphicon-asterisk';
+            }
+            if(autoapprove.indexOf(scope) != -1){
+                return 'glyphicon-ok';
+            }
+            return 'glyphicon-folder-close';
+        }
+        $scope.getScopeTipIndex = function(scope, autoapprove){
+            if(SetUtils.hasItem(autoapprove,'true')){
+                return 0;
+            }
+            if(scope.indexOf('*') != -1){
+                return 1;
+            }
+            if(autoapprove.indexOf(scope) != -1){
+                return 0;
+            }
+            return 2;
+        };
+        $scope.goGroupListOrDetail = function(displayName){
+            var wild = false;
+            var operator = 'eq';
+            if(displayName.indexOf('*') !== -1){
+                wild = true;
+                operator = 'co';
+                displayName = displayName.replace('*','');
+            }
 
+            Group.query({filter: 'displayname ' + operator + ' \'' + displayName + '\''}, function (result) {
+                var find = $filter('filter')(result.resources, {'displayName':displayName}, !wild);
+                if(find.length === 1){
+                    $state.go("group-management-detail",{id: find[0].id});
+                }else if(find.length > 1){
+                    $state.go("group-management",{search: displayName});
+                }else{
+                    AlertService.warning('<strong>UI: </strong>' + displayName + ' is not Group!',{timeout:1000000});
+                }
+            });
+        };
+        $scope.goIdentityProviderDetail = function(originKey){
+            IdentityProvider.query({}, function (result) {
+                var find = $filter('filter')(result, {'originKey':originKey}, true);
+                if(find.length === 1){
+                    $state.go("identity-provider-detail",{id: find[0].id, type: find[0].type});
+                }else{
+                    AlertService.warning('<strong>UI: </strong>' + originKey + ' is not Identity Provider!');
+                }
+            });
+        };
+
+        
         $scope.grants = {};
         angular.forEach(GRANTS,function(grant){
             if(grant.grant){
