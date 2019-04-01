eb41ef7df9f82c2793b5eb0fe8475c669cda7b87
xiaoyao9184 <xiaoyao9184@gmail.com>
2019-04-01 09:11:11 +0800

    Fix script path

--- development/run_quick-uaa-local.sh
+++ development/run_quick-uaa-local.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 repo_root=$(dirname $(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd))
 echo "The resource path is ${repo_root}"
+cd ${repo_root}
 
 echo "Clone quick-uaa-local to ${repo_root}"
 git clone https://github.com/xiaoyao9184/quick-uaa-local ${repo_root}/.quaa
-cd ${repo_root}/.quaa
 
 echo "Change default quaa config"
-cp ${repo_root}/config/quaa/manifests/uaa.yml ${repo_root}/.quaa/manifests/uaa.yml
-cp ${repo_root}/config/quaa/.versions ${repo_root}/.quaa/.versions
+cp ./development/quaa/manifests/uaa.yml ./.quaa/manifests/uaa.yml
+cp ./development/quaa/.versions ./.quaa/.versions
 
 echo "Run quaa up"
-${repo_root}/.quaa/bin/quaa up -ctx uaa
+./.quaa/bin/quaa up -ctx uaa
 
