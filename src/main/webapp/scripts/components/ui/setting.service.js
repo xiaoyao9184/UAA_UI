2fd38c11cb74cb7db51ffbc0ec6c69a21e61b4ce
xiaoyao9184 <xiaoyao9184@gmail.com>
2020-04-13 17:03:44 +0800

    support development env use project name distinction setting localStorage

--- src/main/webapp/scripts/components/ui/setting.service.js
+++ src/main/webapp/scripts/components/ui/setting.service.js
@@ -1,17 +1,21 @@
 'use strict';
 
 angular.module('uaaUIApp')
-    .factory('Setting', function apiService(localStorageService, SETTING) {
+    .factory('Setting', function apiService(localStorageService, $rootElement, ENV, SETTING) {
+        var setting_key = 'setting';
+        if(ENV.includes('dev')){
+            setting_key = setting_key + '_' + $rootElement.attr('ng-app');
+        }
         return {
             version: 7,
             set: function(setting) {
-                localStorageService.set('setting', setting);
+                localStorageService.set(setting_key, setting);
             },
             get: function () {
-                var setting = localStorageService.get('setting');
+                var setting = localStorageService.get(setting_key);
                 if(setting === null || setting.version < this.version){
                     setting = angular.merge({},SETTING);
-                    localStorageService.set('setting', setting);
+                    localStorageService.set(setting_key, setting);
                 }
                 return setting;
             }
